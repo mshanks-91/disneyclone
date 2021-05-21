a2ac35304bf9e3d6fa8b9f59bc42232501329f90
shanks <mshashank1991@gmail.com>
2021-05-21 00:56:06 -0700

    login-page-1

---
+++ firstapp/disney-plus-clone/src/components/Login.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Login() {
+    return (
+        <Container>
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg" />
+            </CTA>
+            <SignUp>
+                Get over here!!
+            </SignUp>
+        </Container>
+    )
+}
+
+export default Login
+
+const Container = styled.div`
+    position: relative;
+    height: calc(100vh - 70px);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:before{
+        background-position: top;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-image: url("/images/login-background.jpg"); 
+        
+        content: "";
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        z-index: -1;
+    }
+
+`
+
+const CTA = styled.div`
+    max-width: 650px;
+    padding: 80px 40px;
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+`
+
+const CTALogoOne = styled.img`
+
+`
+const SignUp = styled.a`
+    width: 100%;
+    background-color: #0063e5;
+    font-weight: bold;
+`
